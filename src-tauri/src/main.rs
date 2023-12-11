5522173339fb4ba1e0026f23665dd5789e523718
s-haya-123 <ganbaru.wakenai@gmail.com>
2023-12-11 23:24:59 +0900

    OSCの受信とパースを追加

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,120 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+extern crate rosc;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+mod vmcp;
+
+use rosc::OscPacket;
+use serde::Serialize;
+use std::net::{SocketAddrV4, UdpSocket};
+use std::str::FromStr;
+use tauri::{App, Manager};
+
+use std::thread;
+
+use vmcp::{Address, BonePosition, Camera, ExtendesParser, Transform};
+
+#[derive(Debug, Serialize)]
+struct Message {
+    time: Vec<f32>,
+    bone: Vec<BonePosition>,
+    camera: Vec<Camera>,
+    root: Vec<Transform>,
+}
+
+impl Message {
+    fn is_empty(&self) -> bool {
+        self.bone.is_empty()
+            && self.camera.is_empty()
+            && self.time.is_empty()
+            && self.root.is_empty()
+    }
+}
+
+fn parse_osc_bundle(packet: OscPacket) -> Message {
+    match packet {
+        OscPacket::Message(_) => Message {
+            bone: vec![],
+            time: vec![],
+            camera: vec![],
+            root: vec![],
+        },
+        OscPacket::Bundle(bundle) => bundle.content.iter().fold(
+            Message {
+                bone: vec![],
+                time: vec![],
+                camera: vec![],
+                root: vec![],
+            },
+            |mut acc, packet| {
+                let m = parse_osc_message(packet);
+                match m {
+                    Ok(address) => {
+                        match address {
+                            Address::Bone(bone) => acc.bone.push(bone),
+                            Address::None => {}
+                        }
+                        acc
+                    }
+                    Err(_) => acc,
+                }
+            },
+        ),
+    }
+}
+
+fn parse_osc_message(packet: &OscPacket) -> Result<Address, String> {
+    match packet {
+        OscPacket::Message(msg) => {
+            return msg.parse();
+        }
+        OscPacket::Bundle(_) => Err("想定と違うデータです".to_string()),
+    }
+}
+
+fn receive_osc(app: &mut App) {
+    let app_handle = app.app_handle();
+
+    thread::spawn(move || {
+        let addr = match SocketAddrV4::from_str("127.0.0.1:39539") {
+            Ok(addr) => addr,
+            Err(_) => panic!("Usage {} IP:PORT", "127.0.0.1:39539"),
+        };
+        let sock = UdpSocket::bind(addr).unwrap();
+        println!("Listening to {}", addr);
+        let mut buf = [0u8; rosc::decoder::MTU];
+        loop {
+            match sock.recv_from(&mut buf) {
+                Ok((size, addr)) => {
+                    println!("Received packet with size {} from: {}", size, addr);
+                    let (_, packet) = rosc::decoder::decode_udp(&buf[..size]).unwrap();
+                    let message = parse_osc_bundle(packet);
+                    if !message.is_empty() {
+                        app_handle.emit_all("OscPacket", &message).unwrap();
+                    }
+                }
+                Err(e) => {
+                    println!("Error receiving from socket: {}", e);
+                    break;
+                }
+            }
+        }
+    });
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            receive_osc(app);
+
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+                window.close_devtools();
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src-tauri/src/vmcp.rs
@@ -0,0 +1,132 @@
+extern crate rosc;
+
+use rosc::{OscMessage, OscType};
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+pub enum BonePositionName {
+    RightHand,
+    RightLowerArm,
+    RightUpperArm,
+    RightShoulder,
+    LeftHand,
+    LeftLowerArm,
+    LeftUpperArm,
+    LeftShoulder,
+    RightUpperLeg,
+    RightLowerLeg,
+    RightToes,
+    LeftUpperLeg,
+    LeftLowerLeg,
+    LeftToes,
+    Hips,
+    Head,
+}
+
+pub enum Address {
+    Bone(BonePosition),
+    None,
+}
+#[derive(Debug, Serialize)]
+struct Position {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+#[derive(Debug, Serialize)]
+struct Rotation {
+    x: f32,
+    y: f32,
+    z: f32,
+    w: f32,
+}
+#[derive(Debug, Serialize)]
+pub struct Transform {
+    position: Position,
+    rotation: Rotation,
+}
+
+#[derive(Debug, Serialize)]
+pub struct BonePosition {
+    pub name: BonePositionName,
+    pub transform: Transform,
+}
+#[derive(Debug, Serialize)]
+pub struct Camera {
+    transform: Transform,
+    fog: f32,
+}
+pub trait ExtendesParser {
+    fn parse(&self) -> Result<Address, String>;
+}
+
+impl ExtendesParser for OscMessage {
+    fn parse(&self) -> Result<Address, String> {
+        match self.addr.as_str() {
+            "/VMC/Ext/Bone/Pos" => match parse_bone(&self.args) {
+                Ok(v) => Ok(Address::Bone(v)),
+                Err(e) => Err(e),
+            },
+            _ => Ok(Address::None),
+        }
+    }
+}
+
+macro_rules! value_impl {
+    ($(($str:expr, $enum:ident)),*) => {
+        fn match_bone_name(bone_string: String) -> Result<BonePositionName, String> {
+            match bone_string.as_str() {
+                $( $str => Ok(BonePositionName::$enum), )*
+                _ => Err("NotDefineBone".to_string()),
+            }
+        }
+    };
+}
+value_impl! {
+    ("Head", Head),
+    ("Hips", Hips),
+    ("RightHand", RightHand),
+    ("RightLowerArm", RightLowerArm),
+    ("RightUpperArm", RightUpperArm),
+    ("RightShoulder", RightShoulder),
+    ("LeftHand", LeftHand),
+    ("LeftLowerArm", LeftLowerArm),
+    ("LeftUpperArm", LeftUpperArm),
+    ("LeftShoulder", LeftShoulder),
+    ("RightUpperLeg", RightUpperLeg),
+    ("RightLowerLeg", RightLowerLeg),
+    ("RightToes", RightToes),
+    ("LeftUpperLeg", LeftUpperLeg),
+    ("LeftLowerLeg", LeftLowerLeg),
+    ("LeftToes", LeftToes)
+}
+
+fn parse_bone(args: &Vec<OscType>) -> Result<BonePosition, String> {
+    let bone_name_result = match_bone_name(args[0].clone().string().unwrap());
+    let bone_name = match bone_name_result {
+        Ok(v) => v,
+        Err(message) => return Err(message),
+    };
+    Ok(BonePosition {
+        name: bone_name,
+        transform: parse_transform(args),
+    })
+}
+
+fn parse_transform(args: &Vec<OscType>) -> Transform {
+    let position = Position {
+        x: args[1].clone().float().unwrap(),
+        y: args[2].clone().float().unwrap(),
+        z: args[3].clone().float().unwrap(),
+    };
+    let rotation = Rotation {
+        x: args[4].clone().float().unwrap(),
+        y: args[5].clone().float().unwrap(),
+        z: args[6].clone().float().unwrap(),
+        w: args[7].clone().float().unwrap(),
+    };
+    Transform {
+        position: position,
+        rotation: rotation,
+    }
+}
